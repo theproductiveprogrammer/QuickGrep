a9c571157e3295ea4c4c18a8f7ced1afdb7aca71
Charles L <charles.lobo@gmail.com>
2021-04-15 14:49:09 +1000

    feat: add help

--- cs/cs.c
+++ cs/cs.c
@@ -1,3 +1,37 @@
-int main(int argc, char* argv[]) {
-  printf("Hello world!");
+#include<stdio.h>
+
+int p(char *msg) { return printf("%s\n", msg); }
+
+struct config {
+  int help;
+};
+
+struct config getConfig(int argc, char* argv[]) {
+  struct config r;
+  r.help = 1;
+
+  return r;
 }
+
+int showHelp() {
+  p("qcs: Quick Code Search");
+  p("$> qcs <regular expression>");
+  p("      Finds all matches in code/text files");
+  p("      Uses sMartCase (ignores case by default)");
+
+  return 0;
+}
+
+int search(struct config config) {
+  return 0;
+}
+
+/*    way/
+ * show help if needed or do the search
+ */
+int main(int argc, char* argv[]) {
+  struct config config = getConfig(argc, argv);
+  if(config.help) return showHelp();
+  else return search(config);
+}
+
